277714a70089906b941dd3626723db52b235c574
szymon_wrobel <szymon.wrobel@pronos.eu>
2021-12-16 11:07:54 +0100

    MAC-48 TS

--- typescript.js
+++ typescript.js
@@ -1,10 +1,4 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: './tsconfig.json',
-        ecmaVersion: 2020,
-        sourceType: 'module',
-    },
     plugins: ['@typescript-eslint'],
     extends: [
         'plugin:import/typescript',
