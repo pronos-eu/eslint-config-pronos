827b79588e34aedcae30f73d9153ccf9f0951704
Szymon Wróbel @pronos-eu <88193604+szymon-wrobel-pronos@users.noreply.github.com>
2022-04-14 16:08:44 +0200

    MAC-48 Sorted core style rules, started TS style section (#10)

--- rules/style.js
+++ rules/style.js
@@ -2,23 +2,14 @@
 
 module.exports = {
     rules: {
-        // Enforce newline before return statement if it's not the only statement in a block
-        // https://eslint.org/docs/rules/padding-line-between-statements
-        'padding-line-between-statements': ['error',
-            {blankLine: 'always', prev: '*', next: 'return'},
-        ],
-
-        // Disallow multiple empty lines in code and padding at start and end of file
-        // https://eslint.org/docs/rules/no-multiple-empty-lines
-        'no-multiple-empty-lines': ['warn', {
-            max: 1,
-            maxBOF: 0,
-            maxEOF: 0,
-        }],
-
         // Require trailing comma in multiline objects, arrays and functions
         // https://eslint.org/docs/rules/comma-dangle
         'comma-dangle': ['error', 'always-multiline'],
+
+        // Ignore warnings regarding newlines before arrow shorthand body
+        // Removed after discussion on #dev-convo
+        // https://eslint.org/docs/rules/implicit-arrow-linebreak
+        'implicit-arrow-linebreak': 'off',
 
         // Require consistent indentation
         // https://eslint.org/docs/rules/indent
@@ -28,23 +19,40 @@ module.exports = {
         // https://eslint.org/docs/rules/semi
         semi: ['error', 'never'],
 
-        // Enforce consistent style of strings
-        // https://eslint.org/docs/rules/quotes
-        quotes: ['error', 'single', {allowTemplateLiterals: true, avoidEscape: true}],
+        // Disallow multiple empty lines in code and padding at start and end of file
+        // https://eslint.org/docs/rules/no-multiple-empty-lines
+        'no-multiple-empty-lines': ['warn', {
+            max: 1,
+            maxBOF: 0,
+            maxEOF: 0,
+        }],
 
         // Require consistent spacing in object literals
         // https://eslint.org/docs/rules/object-curly-spacing
         'object-curly-spacing': ['error', 'never'],
 
-        // Ignore warnings regarding newlines before arrow shorthand body
-        // Removed after discussion on #dev-convo
-        // https://eslint.org/docs/rules/implicit-arrow-linebreak
-        'implicit-arrow-linebreak': 'off',
+        // Enforce newline before return statement if it's not the only statement in a block
+        // https://eslint.org/docs/rules/padding-line-between-statements
+        'padding-line-between-statements': ['error',
+            {blankLine: 'always', prev: '*', next: 'return'},
+        ],
 
-        // To Review:
+        // Enforce consistent style of strings
+        // https://eslint.org/docs/rules/quotes
+        quotes: ['error', 'single', {allowTemplateLiterals: true, avoidEscape: true}],
+
+        // Require spaces around operators
+        // https://eslint.org/docs/rules/space-infix-ops
+        'space-infix-ops': 'error',
+
+        // TODO: Test section, check, document then move above
+        'no-trailing-spaces': ['warn', {
+            skipBlankLines: true,
+        }],
         'no-unneeded-ternary': 'warn',
-        
         'no-unexpected-multiline': 'warn',
+
+        // TODO: Review following rules
         'linebreak-style': 'off',
         'function-paren-newline': 'off',
         'operator-linebreak': ['error', 'after'],
@@ -67,6 +75,5 @@ module.exports = {
         'id-length': ['warn', {min: 2, exceptions: ['_', 'i', 'j', 'k', 'n', 'm']}],
         'no-shadow': 'warn',
         'no-return-assign': 'off',
-        'no-trailing-spaces': 'off',
     },
 }
